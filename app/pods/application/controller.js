8871d62d5e49cf317965e4b4b11e174ac20c66fa
Rafal Biel <rafalbiel77@gmail.com>
2022-12-02 21:49:49 +0100

    added feature with adding task

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 export default class ApplicationController extends Controller {
-  items = [
+  @tracked items = [
     {
       isDone: false,
       label: 'kupić jajka',
@@ -14,4 +16,21 @@ export default class ApplicationController extends Controller {
       label: 'zjeść jajka',
     },
   ];
+
+  @action
+  onInputValue(event) {
+    this.newTaskValue = event.target.value;
+  }
+  @action
+  addNewTask(event) {
+    event.preventDefault();
+
+    this.items = [
+      ...this.items,
+      {
+        isDone: false,
+        label: this.newTaskValue,
+      },
+    ];
+  }
 }
